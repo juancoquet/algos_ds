9d45eba084239681edb10f597ead17948f7ab3a0
juancoquet <juancoquet@gmail.com>
2022-01-24 16:07:05 +0000

    create PriorityQueue class

--- bin_heap.py
+++ bin_heap.py
@@ -46,3 +46,37 @@ class BinaryHeap:
         while i > 0:
             self.percolate_down(i)
             i -= 1
+
+
+class PriorityQueue(BinaryHeap):
+
+    def __init__(self):
+        self.heap = [(0, None)] # (priority, value)
+        self.current_size = 0
+
+    def percolate_up(self, i):
+        while i // 2 > 0:
+            if self.heap_list[i][0] < self.heap_list[i // 2][0]:
+                self.heap_list[i], self.heap_list[i // 2] = self.heap_list[i // 2], self.heap_list[i]
+            i = i // 2
+
+    def percolate_down(self, i):
+        while (i * 2) <= self.current_size:
+            min_child_idx = self.min_child(i)
+            if self.heap_list[i][0] > self.heap_list[min_child_idx][0]:
+                self.heap_list[i], self.heap_list[min_child_idx] = self.heap_list[min_child_idx], self.heap_list[i]
+            i = min_child_idx
+
+    def insert(self, priority, value):
+        self.heap.append((priority, value))
+        self.current_size += 1
+        self.percolate_up(self.current_size)
+
+    def heapify(self, arr):
+        i = len(arr) // 2
+        self.current_size = len(arr)
+        heap_list = [(vx.get_distance(), vx) for vx in arr]
+        self.heap = [(0, None)] + heap_list[:]
+        while i > 0:
+            self.percolate_down(i)
+            i -= 1
